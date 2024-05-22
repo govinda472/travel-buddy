8358101ab2ee9ca7375a16f9a4919b75fde4f089
govinda472 <75463270+govinda472@users.noreply.github.com>
2024-05-22 16:22:48 -0700

    basic template

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+    const loader = document.getElementById('loader');
+    loader.style.display = 'none';
+    document.querySelector('header').style.display = 'block';
+    document.querySelector('main').style.display = 'block';
+    document.querySelector('footer').style.display = 'block';
+});
